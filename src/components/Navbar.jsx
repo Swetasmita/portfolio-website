58d0df2f3731a310062c1474266312c387b2fe51
Swetasmita Chinaray <schinray@gmail.com>
2025-05-22 17:14:55 +0100

    added a codesandbox project  link to the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import logo from "../assets/profile.svg";
 import { FaTwitter } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { PiX } from "react-icons/pi";
+import { SiCodesandbox } from "react-icons/si";
 
 const Navbar = () => {
   return (
@@ -12,14 +11,27 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/swetasmita-chinaray-32984880/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/swetasmita-chinaray-32984880/"
+          target="_blank"
+        >
           <FaLinkedin />
         </a>
-        <a href="https://github.com/Swetasmita?tab=repositories" target="_blank">
-        <FaGithub />      
+        <a
+          href="https://github.com/Swetasmita?tab=repositories"
+          target="_blank"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://codesandbox.io/dashboard/drafts?workspace=ws_961mNTMfJ41ogdLADLJYFR"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiCodesandbox />
         </a>
         <a href="https://x.com/SChinaray12" target="_blank">
-        <FaTwitter />
+          <FaTwitter />
         </a>
       </div>
     </nav>
